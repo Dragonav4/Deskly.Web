d1ab0ae6e3393154a54d2c62887d64c7f05c8fb2
DANYIL DANILIAN <daniliandan100500@gmail.com>
2026-01-05 18:48:57 +0100

    feat(usePermissions): deleted useMemo for permissions calculation

--- src/shared/hooks/usePermissions.ts
+++ src/shared/hooks/usePermissions.ts
@@ -1,18 +1,13 @@
-import { useMemo } from 'react';
-
 const ALLOW_VIEW = 1;
 const ALLOW_CREATE = 2;
 const ALLOW_EDIT = 2;
 const ALLOW_DELETE = 4;
 
-export function usePermissions(actions: number | undefined) {
-    return useMemo(() => {
-        const val = actions ?? 0;
-        return {
-            canView: (val & ALLOW_VIEW) === ALLOW_VIEW,
-            canCreate: (val & ALLOW_CREATE) === ALLOW_CREATE,
-            canEdit: (val & ALLOW_EDIT) === ALLOW_EDIT,
-            canDelete: (val & ALLOW_DELETE) === ALLOW_DELETE,
-        };
-    }, [actions]);
+export function usePermissions(val = 0) {
+    return {
+        canView: (val & ALLOW_VIEW) === ALLOW_VIEW,
+        canCreate: (val & ALLOW_CREATE) === ALLOW_CREATE,
+        canEdit: (val & ALLOW_EDIT) === ALLOW_EDIT,
+        canDelete: (val & ALLOW_DELETE) === ALLOW_DELETE,
+    };
 }
